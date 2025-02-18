db17b3fdc0622194d3e5ab74171422590ff333df
Rashmika Bandara <eg244954@engug.ruh.ac.lk>
2025-02-18 17:52:30 +0530

    Newly added

--- React/src/Components/Navbar/Navbar.jsx
+++ React/src/Components/Navbar/Navbar.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import'./Navbar.css'
 import logo from '../../assets/logo.png'
 
+//New import
+import {link} from 'react-router-dom'
+
+
 const Navbar = () => {
   return (
     <nav className='container'>
       <img src = {logo} alt = "" className='logo'/>
       <ul>
-        <li>Home</li>
+        <li><link to = "/home" >Home</link></li>
         <li>About</li>
         <li>Services</li>
         <li><button className='btn'>Contact Us</button></li>
--- React/src/main.jsx
+++ React/src/main.jsx
@@ -3,8 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+// New import
+import reactDOM from "react-dom/client"
+import { BrowserRouter } from 'react-router-dom'  
+
+
+//Initially was only create root but I added new part
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* Newly add part */}
+    <BrowserRouter>
+       <App />
+    </BrowserRouter>
   </StrictMode>,
 )
